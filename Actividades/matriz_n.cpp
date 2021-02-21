92c5051f2b3f7e8f590c6440bb283b67ab3759e5
Schiavi13 <lufmartinezmu@gmail.com>
2021-02-21 17:20:35 -0500

    Agrega capturar_tamano() a matriz_n.cpp, captura el tamaño de la matriz

---
+++ Actividades/matriz_n.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+
+//Funciones prototipo
+int capturar_tamano(void);
+int * crear_matriz(int);
+void poblar_matriz(int*);
+
+
+int main(){
+    int n = capturar_tamano();
+    printf("tamano: %d", n);
+}
+
+
+//Captura parametros
+int capturar_tamano(){
+    int n = 0;
+    printf("n: \n");
+    while(n<=0){
+        scanf("%d",&n);
+        if(n<=0){
+            printf("Por favor ingrese un entero positivo");
+        }
+    }
+    return n;
+}
+
+int * crear_matriz(int n){
+    int *matriz = (int*) malloc(n * sizeof(int));
+    return(matriz);
+}
