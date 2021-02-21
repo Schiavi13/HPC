cee2c8238601420558e9b3e2a61971dbe8b784b8
Schiavi13 <lufmartinezmu@gmail.com>
2021-02-21 18:15:04 -0500

    corrige crear_matriz() en matriz_n.cpp, con esto se crea la matriz bidimensional

--- Actividades/matriz_n.cpp
+++ Actividades/matriz_n.cpp
@@ -4,17 +4,15 @@
 
 //Funciones prototipo
 int capturar_tamano(void);
-int * crear_matriz(int);
+int ** crear_matriz(int);
 void poblar_matriz(int*);
-
 
 int main(){
     int n = capturar_tamano();
-    int *matriz = crear_matriz(n);
+    int **matriz = crear_matriz(n);
 }
 
-
-//Captura parametros
+//Captura el tamaño de la matriz
 int capturar_tamano(){
     int n = 0;
     printf("n: \n");
@@ -27,7 +25,11 @@ int capturar_tamano(){
     return n;
 }
 
-int * crear_matriz(int n){
-    int *matriz = (int*) malloc(n * sizeof(int));
+//Reserva el espacio en memoria para la matriz
+int ** crear_matriz(int n){
+    int **matriz = (int**) malloc(n * sizeof(int));
+    for(int i=0;i<n;i++){
+        matriz[i] = (int *) malloc(n * sizeof(int));
+    }
     return(matriz);
 }
