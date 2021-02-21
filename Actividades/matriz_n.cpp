f1d7636b27d525af7072c8a22da77358c4e95c30
Schiavi13 <lufmartinezmu@gmail.com>
2021-02-21 18:24:30 -0500

    comenta logica de crear_matriz() en matriz_n.cpp

--- Actividades/matriz_n.cpp
+++ Actividades/matriz_n.cpp
@@ -27,6 +27,18 @@ int capturar_tamano(){
 
 //Reserva el espacio en memoria para la matriz
 int ** crear_matriz(int n){
+    /*Se crea un apuntador a apuntadores, estos apuntadores apuntaran a las 'columnas'
+    que seran espacios de memoria reservados para los datos de cada fila.
+
+    m = {p1, p2, p3,...,pn}
+    donde p1 -> {a11, a12, a13,..., a1n}
+          p2 -> {a21, a22, a23,..., a2n}
+          .
+          .
+          .
+          pn -> {an1, an2, an3,..., ann}
+
+    */
     int **matriz = (int**) malloc(n * sizeof(int));
     for(int i=0;i<n;i++){
         matriz[i] = (int *) malloc(n * sizeof(int));
