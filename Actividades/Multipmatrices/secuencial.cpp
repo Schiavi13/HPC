ce85f098149585533f533f522df3b37095c50a47
Schiavi13 <lufmartinezmu@gmail.com>
2021-03-17 14:17:11 -0500

    Comenta impresiones en pantalla de las matrices

--- Actividades/Multipmatrices/secuencial.cpp
+++ Actividades/Multipmatrices/secuencial.cpp
@@ -22,14 +22,14 @@ int main(){
     matrizResultado = crear_matriz(matrizResultado, n);
     poblar_matriz(matriz1, n);
     poblar_matriz(matriz2, n);
-    mostrar_matriz(matriz1, n);
-    mostrar_matriz(matriz2, n);
+    //mostrar_matriz(matriz1, n);
+    //mostrar_matriz(matriz2, n);
     clock_t inicio = clock();
     matrizResultado = multiplicar_matrices(matriz1,matriz2,matrizResultado,n);
     clock_t fin = clock();
     float segundos = (float)(fin - inicio) / CLOCKS_PER_SEC;
     printf("La ejecucion ha tomado %.4f segundos\n", segundos);
-    mostrar_matriz(matrizResultado,n);
+    //mostrar_matriz(matrizResultado,n);
     free(matriz1);
     free(matriz2);
     free(matrizResultado);
