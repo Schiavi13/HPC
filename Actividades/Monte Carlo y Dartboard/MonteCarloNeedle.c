269be0313e22d37b27c0140abe3940851d7a6e19
Schiavi13 <lufmartinezmu@gmail.com>
2021-04-29 15:02:52 -0500

    Parametriza monte carlo needle en C

--- Actividades/Monte Carlo y Dartboard/MonteCarloNeedle.c
+++ Actividades/Monte Carlo y Dartboard/MonteCarloNeedle.c
@@ -5,15 +5,15 @@
 
 
 //Funciones prototipo
-int monte_carlo();
-//Se asume una longitud de aguja de 1, y una distancia entre las l�neas de 2
-#define L 2.0
-#define ITERACIONES 100000
+int monte_carlo(float,int);
+
 #ifndef MPI
 #define M_PI 3.14159265358979323846
 #endif
 
-int main(){
+int main(int argc, char *argv[]){
+    float l = (float)atof(argv[1]);
+    int iteraciones = (int)atoi(argv[2]); 
     float p, pi;
     int n_cruzados;
     //Define la semilla usando el reloj del sistema
@@ -21,10 +21,10 @@ int main(){
 
     //Calcula la probabilidad de que las agujas se crucen
     clock_t inicio = clock();
-    n_cruzados = monte_carlo();
-    p = n_cruzados/(float) ITERACIONES;
+    n_cruzados = monte_carlo(l,iteraciones);
+    p = n_cruzados/(float) iteraciones;
     //Infiere Pi desde la probabilidad, sabiendo P = 2*longitud/Pi*Distancia
-    pi = 2.0/(p*L);
+    pi = 2.0/(p*l);
     clock_t fin = clock();
     float segundos = (float)(fin - inicio) / CLOCKS_PER_SEC;
     printf("Probabilidad: %f\n",p);
@@ -33,17 +33,17 @@ int main(){
     return 0;
 }
 
-int monte_carlo(){
+int monte_carlo(float l, int iteraciones){
     float x, theta, n;
     int n_cruzados = 0;
-    for(int i=0; i<ITERACIONES; i++){
+    for(int i=0; i<iteraciones; i++){
         //Genera el estado (X, Theta)
-        x = (float)rand()/(float)(RAND_MAX/L);
+        x = (float)rand()/(float)(RAND_MAX/l);
         theta = (float)rand()/(float)(RAND_MAX/M_PI);
 
         //Calcula si la aguja cruza
         n = sin(theta)/2.0;
-        if(x+n >= L || x-n<= 0){
+        if(x+n >= l || x-n<= 0){
             n_cruzados++;
         }
     }
